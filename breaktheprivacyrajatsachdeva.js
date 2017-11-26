06016bdda4c33670360847e02310d405764801ff
breaktheprivacy <33506692+breaktheprivacy@users.noreply.github.com>
2017-11-26 08:33:53 +0000

    Add files via upload

---
+++ breaktheprivacyrajatsachdeva.js
@@ -0,0 +1,1 @@
+<script>var voicelist=responsiveVoice.getVoices(),vselect=$("#voiceselection");$.each(voicelist, function(){vselect.append($("<option />").val(this.name).text(this.name))});</script>
